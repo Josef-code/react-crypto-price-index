6699f1db9b35fa1f042672944bdc2f154c4df56d
josef-code <jbbliss9@gmail.com>
2023-10-15 00:27:15 +0100

    add chakra-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+
 import "./index.css";
 
 function App() {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-full text-white p-8">
-        <h1 className="text-2xl">Crypto Currency Rates App</h1>
-        <h2>Track the real-time exchange rates of over 100 crypto currencies.</h2>
+        <h1 className="text-5xl">Crypto Currency Rates App</h1>
+        <h2 className="text-2xl p-2">
+          Track the real-time exchange rates of over 100 crypto currencies.
+        </h2>
       </div>
     </>
   );
