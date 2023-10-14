8b225f9a24f62193002879caf8a722596268f360
josef-code <jbbliss9@gmail.com>
2023-10-14 22:27:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-full text-white p-8">
+        <h1 className="text-2xl">Crypto Currency Rates App</h1>
+        <h2>Track the real-time exchange rates of over 100 crypto currencies.</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
